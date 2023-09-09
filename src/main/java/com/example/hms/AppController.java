210567046ff4345dec782a6494a037aebf0c56fb
sanjib <sanjib.bhuyan786@gmail.com>
2023-09-09 10:32:50 +0530

    first_commit

---
+++ src/main/java/com/example/hms/AppController.java
@@ -0,0 +1,15 @@
+package com.example.hms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+public class AppController {
+
+    @GetMapping
+    public String getData(){
+        return "welcome to docker";
+    }
+}
